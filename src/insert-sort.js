7cd4f4bc79b08490b58a314cee58e04bc9a9951d
薛纪洪 <632565970@qq.com>
2019-05-10 02:23:25 +0800

    optimization insert sort

--- src/insert-sort.js
+++ src/insert-sort.js
@@ -3,14 +3,19 @@ const { sortRule } = utils
 
 function insertSort(arrs, sort = sortRule) {
   const len = arrs.length
+
   for (let i = 1; i < len; i++) {
+
+    let e = arrs[i]
+    let j;
     for (let j = i; j > 0; j--) {
-      if (sort(arrs[j], arrs[j - 1])) {
-        [arrs[j], arrs[j-1]] = [arrs[j-1], arrs[j]]
+      if (sort(e, arrs[j - 1])) {
+        arrs[j] = arrs[j - 1]
       } else {
         break;
       }
     }
+    arrs[j] = e
   }
 }
 
