392c2d48a8c5080dff64ece300c9deb29837695b
薛纪洪 <632565970@qq.com>
2019-05-10 11:35:24 +0800

    shell sort

---
+++ src/shell-sort.js
@@ -0,0 +1,28 @@
+
+const utils = require('../utils')
+const { sortRule } = utils
+
+function shellSort(array, sort = sortRule) {
+  let length = array.length;
+  let gap = Math.floor(length / 2);
+
+  while (gap > 0) {
+
+    for (let i = gap; i < length; i++) {
+      let e = array[i]
+      let j;
+      for (j = i; j > 0; j-=gap) {
+        if(sort(e,array[j-gap])){
+          array[j] = array[j-gap]
+        }else{
+          break
+        }
+      }
+      array[j] = e
+    }
+
+    gap = Math.floor(gap / 2)
+  }
+}
+
+module.exports = shellSort
