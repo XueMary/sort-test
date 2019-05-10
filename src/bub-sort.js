d5602a2bbcf5da24919187c334732b01eb4d71a8
薛纪洪 <632565970@qq.com>
2019-05-10 10:00:26 +0800

    optimization bubble sort

--- src/bub-sort.js
+++ src/bub-sort.js
@@ -5,9 +5,14 @@ function bubSortt(arrs, sort = sortRule) {
   const len = arrs.length
 
   for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len-1-i; j++) {
-      if(sort(arrs[j+1],arrs[j])){
-        [arrs[j+1],arrs[j]] = [arrs[j],arrs[j+1]]
+    let j;
+    let e = arrs[i]
+    for (j = 0; j < len-1-i; j++) {
+      if(sort(arrs[j+1],e)){
+        arrs[j] = arrs[j+1]
+      }else{
+        arrs[j] = e
+        e = arrs[j+1]
       }
     }
   }
