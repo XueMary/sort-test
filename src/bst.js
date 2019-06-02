a1c2987b9499022a451bd02eff0d9a13b6f1e6b3
薛纪洪 <632565970@qq.com>
2019-06-02 17:35:26 +0800

    order

--- src/bst.js
+++ src/bst.js
@@ -38,6 +38,70 @@ class BST {
     return node
   }
 
+  search(key){
+    return this._search(this.root, key)
+  }
+
+  _search(node, key){
+    if(node == null){
+      return null
+    }
+    if(node.key > key){
+      return this._search(node.left, key)
+    } else if(node.key < key){
+      return this._search(node.right, key)
+    } else if(node.key == key){
+      return value
+    }
+  }
+
+  contain(key){
+    return this._contain(this.root, key)
+  }
+
+  _contain(node, key){
+    if(node == null){
+      return false
+    }
+    if(node.key > key){
+      return this._contain(node.left, key)
+    } else if(node.key < key){
+      return this._contain(node.right, key)
+    } else if(node.key == key){
+      return true
+    }
+  }
+
+  preOrder(){
+    this._preOrder(this.root)
+  }
+
+  _preOrder(node){
+    console.log(node.value)
+    this._preOrder(node.left)
+    this._preOrder(node.right)
+  }
+
+  inOrder(){
+    this._inOrder(this.root)
+  }
+
+  _inOrder(node){
+    this._inOrder(node.left)
+    console.log(node.value)
+    this._inOrder(node.right)
+  }
+
+  postOrder(){
+    this._postOrder(this.root)
+  }
+
+  _postOrder(node){
+    this._postOrder(node.left)
+    this._postOrder(node.right)
+    console.log(node.value)
+  }
+
   size() {
     return this.count
   }
