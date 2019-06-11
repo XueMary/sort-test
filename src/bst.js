6535c7206f041a6819220d85822c38057c80c6a4
薛纪洪 <632565970@qq.com>
2019-06-11 14:02:29 +0800

    .

--- src/bst.js
+++ src/bst.js
@@ -38,75 +38,203 @@ class BST {
     return node
   }
 
-  search(key){
+  search(key) {
     return this._search(this.root, key)
   }
 
-  _search(node, key){
-    if(node == null){
+  _search(node, key) {
+    if (node == null) {
       return null
     }
-    if(node.key > key){
+    if (node.key > key) {
       return this._search(node.left, key)
-    } else if(node.key < key){
+    } else if (node.key < key) {
       return this._search(node.right, key)
-    } else if(node.key == key){
+    } else if (node.key == key) {
       return value
     }
   }
 
-  contain(key){
+  contain(key) {
     return this._contain(this.root, key)
   }
 
-  _contain(node, key){
-    if(node == null){
+  _contain(node, key) {
+    if (node == null) {
       return false
     }
-    if(node.key > key){
+    if (node.key > key) {
       return this._contain(node.left, key)
-    } else if(node.key < key){
+    } else if (node.key < key) {
       return this._contain(node.right, key)
-    } else if(node.key == key){
+    } else if (node.key == key) {
       return true
     }
   }
 
-  preOrder(){
+  preOrder() {
     this._preOrder(this.root)
   }
 
-  _preOrder(node){
+  _preOrder(node) {
     console.log(node.value)
     this._preOrder(node.left)
     this._preOrder(node.right)
   }
 
-  inOrder(){
+  inOrder() {
     this._inOrder(this.root)
   }
 
-  _inOrder(node){
+  _inOrder(node) {
     this._inOrder(node.left)
     console.log(node.value)
     this._inOrder(node.right)
   }
 
-  postOrder(){
+  postOrder() {
     this._postOrder(this.root)
   }
 
-  _postOrder(node){
+  _postOrder(node) {
     this._postOrder(node.left)
     this._postOrder(node.right)
     console.log(node.value)
+  }
+
+  levelOrder() {
+    let queue = []
+    queue.push(this.root)
+
+    while (queue.length) {
+      const node = queue.shift()
+      console.log(node.value)
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+  }
+
+  minimum() {
+    if (this.count !== 0) {
+      const miniNode = _minimum(this.root)
+      return miniNode.key
+    }
+  }
+
+  _minimum(node) {
+    if (node.left == null) {
+      return node
+    }
+    return this._minimum(node.left)
+  }
+
+  maximum() {
+    if (this.count !== 0) {
+      const miniNode = _maximum(this.root)
+      return miniNode.key
+    }
+  }
+
+  _maximum(node) {
+    if (node.right == null) {
+      return node
+    }
+    return this._maximum(node.right)
+  }
+
+  removeMin() {
+    let min = null
+    if (this.root) {
+      min = this._removeMin(this.root)
+    }
+    if(min){
+      this.count--
+    }
+    return min
+  }
+
+  _removeMin(node) {
+    if (node == null) {
+      return node
+    }
+
+    if (node.left) {
+      return this._removeMin(node.left)
+    } else {
+      let min = node
+      node = node.right
+      return min
+    }
+  }
+
+  removeMax() {
+    let max = null
+    if (this.root) {
+      max = this._removeMax(this.root)
+    }
+    if(max){
+      this.count--
+    }
+    return max
+  }
+
+  _removeMin(node) {
+    if (node == null) {
+      return node
+    }
+
+    if (node.right) {
+      return this._removeMax(node.right)
+    } else {
+      let max = node
+      node = node.left
+      return max
+    }
+  }
+
+  remove(key) {
+    let removeNode = null
+    removeNode = this._remove(this.root, key)
+    if(removeNode){
+      this.count--
+    }
+    return removeNode
+  }
+
+  _remove(node, key) {
+    if(node == null){
+      return null
+    }
+    if (node.key > key) {
+      return this._remove(node.left, key)
+    } else if (node.key < key) {
+      return this._remove(node.right, key)
+    } else if (node.key == key) {
+      const {key, value} = node
+      if(node.left == null && node.right == null){
+        node = null
+      }else if(node.left == null){
+        node = node.right
+      } else if(node.right == null){
+        node = node.left
+      }else{
+        const newNode = this._removeMin(node.right)
+        const { key, value, right } = newNode
+        node.key = key
+        node.value = value
+        if (right) {
+          node.right = right
+        }
+      }
+      return {key, value}
+    }
   }
 
   size() {
     return this.count
   }
 
-  isEmpty() {
+  empty() {
     return this.count == 0
   }
 }
